54f00c47e957a95777503fcf50f454a05cef5447
Akikukeo1 (Keisuke Ishii) <256033008+Akikukeo1@users.noreply.github.com>
2026-04-15 19:28:31 +0900

    Update ci_intentional_fmt_fail.go
    Signed-off-by: Akikukeo1 (Keisuke Ishii) <256033008+Akikukeo1@users.noreply.github.com>

--- src-go/ci_intentional_fmt_fail.go
+++ src-go/ci_intentional_fmt_fail.go
@@ -1,4 +1,5 @@
 package main
+package adfsi jdfijo asd jfsoidfjasiojfi oaf ioajsdfioj
 
 // このファイルは意図的にフォーマットを崩してあり、
 // `gofumpt -l -e src-go` などのフォーマットチェックで検出されます。
@@ -6,8 +7,7 @@ package main
 // TODO: CIテストが完了したらこのファイルを削除してください。
 import "fmt"
 
-func          fmtFail(
 
-) {
-	fmt.Println( "This file intentionally fails gofumpt formatting check " )
-}
+func fmtFail() { fmt.Println("This file intentionally fails gofumpt formatting check") }
+未使用関数aaaaaaaaaaaaaaaaaaaaa
+func unusedFunctionAAAAAAAAAAAAAAA() {}
