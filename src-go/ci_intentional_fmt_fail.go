6b7aa250050586960c410ab2ae976b1aa9c9ddff
Akikukeo1 (Keisuke Ishii) <256033008+Akikukeo1@users.noreply.github.com>
2026-04-15 18:56:23 +0900

    わざとエラーを出す
    Signed-off-by: Akikukeo1 (Keisuke Ishii) <256033008+Akikukeo1@users.noreply.github.com>

---
+++ src-go/ci_intentional_fmt_fail.go
@@ -0,0 +1,9 @@
+package main
+
+// このファイルは意図的にフォーマットを崩してあり、
+// `gofumpt -l -e src-go` などのフォーマットチェックで検出されます。
+// FIXME: CIテストが完了したらこのファイルを削除してください。
+// TODO: CIテストが完了したらこのファイルを削除してください。
+import "fmt"
+
+func fmtFail() { fmt.Println("This file intentionally fails gofumpt formatting check") }
