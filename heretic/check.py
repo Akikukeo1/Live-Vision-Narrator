54c0a670e60dce2a20b9de080ea6f3c59aee7d3e
Akikukeo1 (Keisuke Ishii) <256033008+Akikukeo1@users.noreply.github.com>
2026-04-18 20:59:51 +0900

    Update check.py

--- heretic/check.py
+++ heretic/check.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 
 print("torch:", torch.__version__)
 
@@ -6,3 +7,7 @@ print("cuda available:", torch.cuda.is_available())
 
 if torch.cuda.is_available():
     print("device name:", torch.cuda.get_device_name(0))
+
+print(f"PyTorch CUDA version: {torch.version.cuda}")
+
+print(f"torchvision version: {torchvision.__version__}")
